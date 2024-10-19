34226c4a51cc7742854114e24071a6dc319d52c6
gustavodiniz <gustavodinniz.dev@gmail.com>
2024-10-19 16:20:10 -0300

    feat: adding spe-products-api

---
+++ sbe-products-api/src/main/java/io/github/gustavodinniz/sbe_products_api/SbeProductsApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.gustavodinniz.sbe_products_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbeProductsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbeProductsApiApplication.class, args);
+	}
+
+}
---
+++ sbe-products-api/src/test/java/io/github/gustavodinniz/sbe_products_api/SbeProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gustavodinniz.sbe_products_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbeProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
